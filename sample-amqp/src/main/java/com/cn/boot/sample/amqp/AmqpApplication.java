5fbbfd324b7b8b17059e15f0ec7f2dca0e0946bf
chennan <327919006@qq.com>
2019-06-03 09:17:22 +0800

    整合RabbitMQ

---
+++ sample-amqp/src/main/java/com/cn/boot/sample/amqp/AmqpApplication.java
@@ -0,0 +1,14 @@
+package com.cn.boot.sample.amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Chen Nan
+ */
+@SpringBootApplication
+public class AmqpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmqpApplication.class, args);
+    }
+}
---
+++ sample-amqp/src/main/java/com/cn/boot/sample/amqp/test1/AmqpDemo1.java
@@ -0,0 +1,35 @@
+package com.cn.boot.sample.amqp.test1;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * <p>Title:</p>
+ * <p>Description:</p>
+ *
+ * @author Chen Nan
+ * @date 2019/6/2.
+ */
+@Component
+public class AmqpDemo1 {
+
+    public void init() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+
+        Connection connection = factory.newConnection();
+
+        Channel channel = connection.createChannel();
+
+        String content = "Hello RabbitMQ!";
+        channel.basicPublish("", "test001", null, content.getBytes());
+
+    }
+}
