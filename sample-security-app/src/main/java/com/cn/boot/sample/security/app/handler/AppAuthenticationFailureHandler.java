31b2bc4a2b67972699a15c8d23f8c9ec387b6306
chennan <327919006@qq.com>
2019-07-12 09:30:21 +0800

    整合SpringOAuth

--- sample-security-app/src/main/java/com/cn/boot/sample/security/app/handler/AppAuthenticationFailureHandler.java
+++ sample-security-app/src/main/java/com/cn/boot/sample/security/app/handler/AppAuthenticationFailureHandler.java
@@ -1,10 +1,7 @@
 package com.cn.boot.sample.security.app.handler;
 
 import cn.hutool.json.JSONUtil;
-import com.cn.boot.sample.security.core.config.properties.SecurityProperties;
-import com.cn.boot.sample.security.core.enums.LoginTypeEnum;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -22,19 +19,12 @@ import java.io.IOException;
 @Component
 public class AppAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
-    @Autowired
-    private SecurityProperties securityProperties;
-
     @Override
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
         log.info("【登录失败】");
-        if (securityProperties.getBrowser().getLoginType().equals(LoginTypeEnum.JSON)) {
-            response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-            response.getWriter().print(JSONUtil.toJsonStr(new Error(exception.getMessage())));
-        } else {
-            super.onAuthenticationFailure(request, response, exception);
-        }
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().print(JSONUtil.toJsonStr(new Error(exception.getMessage())));
     }
 }
