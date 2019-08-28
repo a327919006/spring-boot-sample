fa946a597ecfea208754dcd19eda0b5a7b97d919
chennan <chennan@onemt.com.cn>
2019-08-28 16:15:10 +0800

    数据库初始化SQL语句

---
+++ sql/boot-sample2-init.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.6.21 : Database - boot_sample2
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`boot_sample2` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `boot_sample2`;
+
+/*Table structure for table `teacher` */
+
+CREATE TABLE `teacher` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `age` int(5) DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
