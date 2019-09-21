9c6e233ea62effe03a6d3e9647e8fcd59c801338
chennan <327919006@qq.com>
2019-09-21 20:21:12 +0800

    整合RocketMQ

---
+++ sample-rocketmq/src/main/java/com/cn/boot/sample/rocketmq/constant/MqConstant.java
@@ -0,0 +1,13 @@
+package com.cn.boot.sample.rocketmq.constant;
+
+/**
+ * @author Chen Nan
+ */
+public class MqConstant {
+    private MqConstant() {
+        throw new IllegalStateException("can't init MqConstant");
+    }
+
+    public static final String NAME_SERVER_ADDRESS = "localhost:9876";
+
+}
