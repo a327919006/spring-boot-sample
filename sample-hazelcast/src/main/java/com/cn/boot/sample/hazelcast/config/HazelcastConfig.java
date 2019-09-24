ae20a1a37f355d15e215ab30900cae0e2dc5f4ac
chennan <chennan@onemt.com.cn>
2019-09-24 11:51:54 +0800

    整合Hazelcast分布式缓存

--- sample-hazelcast/src/main/java/com/cn/boot/sample/hazelcast/config/HazelcastConfig.java
+++ sample-hazelcast/src/main/java/com/cn/boot/sample/hazelcast/config/HazelcastConfig.java
@@ -1,9 +1,6 @@
 package com.cn.boot.sample.hazelcast.config;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.EvictionPolicy;
-import com.hazelcast.config.MapConfig;
-import com.hazelcast.config.MaxSizeConfig;
+import com.hazelcast.config.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,14 +12,37 @@ public class HazelcastConfig {
 
     @Bean
     public Config hazelCastConfig() {
+        // 配置监控中心
+        ManagementCenterConfig centerConfig = new ManagementCenterConfig()
+                .setEnabled(true)
+                .setUrl("http://localhost:8080/hazelcast-mancenter");
+
+        // map配置
+        MapConfig mapConfig = new MapConfig()
+                .setName("default")
+                .setBackupCount(4)
+                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                .setEvictionPolicy(EvictionPolicy.LRU)
+                .setTimeToLiveSeconds(-1);
+
+        // 网络配置
+        NetworkConfig networkConfig = new NetworkConfig().setJoin(
+                new JoinConfig()
+                        .setMulticastConfig(new MulticastConfig().setEnabled(false))
+                        .setTcpIpConfig(
+                                new TcpIpConfig()
+                                        .setEnabled(true)
+                                        .addMember("192.168.6.44:5701")
+                                        .addMember("192.168.6.44.5702")
+                                        .addMember("192.168.6.44.5703")
+                                        .addMember("192.168.6.44.5704")
+                        ));
+
         Config config = new Config();
-        config.setInstanceName("hazelcast-instance").
-                addMapConfig(
-                        new MapConfig().
-                                setName("configuration")
-                                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-                                .setEvictionPolicy(EvictionPolicy.LRU)
-                                .setTimeToLiveSeconds(-1));
+        config.setInstanceName("hazelcast-instance")
+                .addMapConfig(mapConfig)
+                .setManagementCenterConfig(centerConfig)
+                .setNetworkConfig(networkConfig);
         return config;
     }
 }
