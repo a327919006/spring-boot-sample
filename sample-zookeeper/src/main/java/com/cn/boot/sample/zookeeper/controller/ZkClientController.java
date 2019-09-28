78491223ed82eb6dfc69ccf97d607b8a7d5247bd
chennan <327919006@qq.com>
2019-09-28 20:44:56 +0800

    整合Zookeeper-使用ZkClient

---
+++ sample-zookeeper/src/main/java/com/cn/boot/sample/zookeeper/controller/ZkClientController.java
@@ -0,0 +1,32 @@
+package com.cn.boot.sample.zookeeper.controller;
+
+import com.cn.boot.sample.api.model.Constants;
+import com.cn.boot.sample.zookeeper.zkclient.ZkClientUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Chen Nan
+ */
+@Slf4j
+@RestController
+@RequestMapping("/zkclient")
+@Api(tags = "ZkClient测试", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class ZkClientController {
+
+    @Autowired
+    private ZkClientUtil zkClientUtil;
+
+    @ApiOperation("创建临时节点")
+    @PostMapping("/node/ephemeral")
+    public String set(String node) {
+        zkClientUtil.createEphemeral(node);
+        return Constants.MSG_SUCCESS;
+    }
+}
---
+++ sample-zookeeper/src/main/java/com/cn/boot/sample/zookeeper/zkclient/ZkClientUtil.java
@@ -0,0 +1,47 @@
+package com.cn.boot.sample.zookeeper.zkclient;
+
+import cn.hutool.core.util.IdUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.I0Itec.zkclient.IZkChildListener;
+import org.I0Itec.zkclient.ZkClient;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+/**
+ * @author Chen Nan
+ */
+@Slf4j
+@Component
+public class ZkClientUtil {
+    private static ZkClient zkClient;
+    public static final String PARENT_NODE = "/test";
+
+    @PostConstruct
+    public void init() {
+        zkClient = new ZkClient("localhost:2181", 3000);
+
+        boolean exists = zkClient.exists(PARENT_NODE);
+        if (!exists) {
+            zkClient.createPersistent(PARENT_NODE);
+        }
+
+        zkClient.subscribeChildChanges(PARENT_NODE, new IZkChildListener() {
+            @Override
+            public void handleChildChange(String parentPath, List<String> currentChilds) throws Exception {
+                log.info("【ZkClientUtil】节点变更, parentPath={}, currentChilds={}", parentPath, currentChilds);
+            }
+        });
+
+        zkClient.createEphemeral(PARENT_NODE + "/" + IdUtil.objectId());
+    }
+
+    public void createEphemeral(String node) {
+        String newNode = PARENT_NODE + "/" + node;
+        if (!zkClient.exists(newNode)) {
+            zkClient.createEphemeral(newNode);
+        }
+    }
+
+}
