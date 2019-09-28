c9b8708126f7f44c4ef378fc3af74da1d0c07e1e
chennan <327919006@qq.com>
2019-09-28 19:12:10 +0800

    整合RocketMQ-根据tag消费

--- sample-rocketmq/src/main/java/com/cn/boot/sample/rocketmq/original/tag/test4/TagConsumer.java
+++ sample-rocketmq/src/main/java/com/cn/boot/sample/rocketmq/original/tag/test4/TagConsumer.java
@@ -1,9 +1,10 @@
-package com.cn.boot.sample.rocketmq.original.async.test3;
+package com.cn.boot.sample.rocketmq.original.tag.test4;
 
 import cn.hutool.json.JSONUtil;
 import com.cn.boot.sample.rocketmq.constant.MqConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
@@ -13,23 +14,24 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.List;
 
 
 /**
- * 异步发送
+ * 根据tag消费
  *
  * @author Chen Nan
  */
 @Slf4j
-//@Component
-public class AsyncConsumer {
-    public static final String TAG = "test3";
+@Component
+public class TagConsumer {
+    public static final String TAG = "test4";
 
     private static DefaultMQPushConsumer consumer;
 
     @PostConstruct
     public void init() throws MQClientException {
-        log.info("【AsyncConsumer】init");
+        log.info("【TagConsumer】init");
 
         consumer = new DefaultMQPushConsumer(TAG + "_consumer_group");
 
@@ -37,14 +39,15 @@ public class AsyncConsumer {
 
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
 
-        consumer.subscribe(TAG + "_topic", "*");
+        consumer.subscribe(TAG + "_topic", "test4_1||test4_2");
 
         consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
             MessageExt msg = msgs.get(0);
-            log.info("【AsyncConsumer】msg={}", JSONUtil.toJsonStr(msg));
+            log.info("【TagConsumer】msg={}", JSONUtil.toJsonStr(msg));
+            // 处理业务
             String body = new String(msg.getBody());
-            log.info("【AsyncConsumer】body={}", body);
 
+            log.info("【TagConsumer】body={}", body);
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
 
@@ -53,7 +56,7 @@ public class AsyncConsumer {
 
     @PreDestroy
     public void destroy() {
-        log.info("【AsyncConsumer】destroy");
+        log.info("【TagConsumer】destroy");
         consumer.shutdown();
     }
 }
--- sample-rocketmq/src/main/java/com/cn/boot/sample/rocketmq/original/tag/test4/TagProducer.java
+++ sample-rocketmq/src/main/java/com/cn/boot/sample/rocketmq/original/tag/test4/TagProducer.java
@@ -1,37 +1,34 @@
-package com.cn.boot.sample.rocketmq.original.async.test3;
+package com.cn.boot.sample.rocketmq.original.tag.test4;
 
-import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.json.JSONUtil;
 import com.cn.boot.sample.rocketmq.constant.MqConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.exception.RemotingException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 /**
- * 异步发送
+ * 根据tag消费
  *
  * @author Chen Nan
  */
 @Slf4j
-//@Component
-public class AsyncProducer {
-    public static final String TAG = "test3";
+@Component
+public class TagProducer {
+    public static final String TAG = "test4";
 
     private static DefaultMQProducer producer;
 
     @PostConstruct
     public void init() throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
-        log.info("【AsyncProducer】init");
+        log.info("【TagProducer】init");
 
         producer = new DefaultMQProducer(TAG + "_producer_group");
 
@@ -39,32 +36,24 @@ public class AsyncProducer {
 
         producer.start();
 
-
-        for (int i = 0; i < 10; i++) {
+        // 发送五条消息
+        for (int i = 0; i < 5; i++) {
             byte[] body = ("Hello RocketMQ! " + TAG + "_" + i).getBytes();
             Message message = new Message();
             message.setTopic(TAG + "_topic"); // 主题
-            message.setTags(TAG); // 标签
+            message.setTags(TAG + "_" + i); // 标签
             message.setKeys(TAG + "_" + i); // 消息唯一标识，用户自定义
             message.setBody(body);
 
-            producer.send(message, new SendCallback() {
-                @Override
-                public void onSuccess(SendResult sendResult) {
-                    log.info("【AsyncProducer】sendResult = {}", JSONUtil.toJsonStr(sendResult));
-                }
-
-                @Override
-                public void onException(Throwable throwable) {
-                    log.info("【AsyncProducer】error = {}", throwable.getMessage(), throwable);
-                }
-            });
+            SendResult sendResult = producer.send(message);
+            // {"traceOn":true,"regionId":"DefaultRegion","msgId":"C0A81F411BEC18B4AAC26B2653F70000","messageQueue":{"queueId":3,"topic":"test1_topic","brokerName":"Hasee-PC"},"sendStatus":"SEND_OK","queueOffset":0,"offsetMsgId":"C0A81F4100002A9F0000000000000C4A"}
+            log.info("【TagProducer】sendResult = {}", JSONUtil.toJsonStr(sendResult));
         }
     }
 
     @PreDestroy
     public void destroy() throws Exception {
-        log.info("【AsyncProducer】destroy");
+        log.info("【TagProducer】destroy");
         producer.shutdown();
     }
 }
