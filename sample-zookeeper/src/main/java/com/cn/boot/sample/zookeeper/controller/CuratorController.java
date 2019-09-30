be21f4b201453623ef44a9de9c1b7c7411b6d1e7
chennan <chennan@onemt.com.cn>
2019-09-30 10:00:21 +0800

    整合Zookeeper，使用Curator框架

---
+++ sample-zookeeper/src/main/java/com/cn/boot/sample/zookeeper/controller/CuratorController.java
@@ -0,0 +1,32 @@
+package com.cn.boot.sample.zookeeper.controller;
+
+import com.cn.boot.sample.api.model.Constants;
+import com.cn.boot.sample.zookeeper.curator.CuratorUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Chen Nan
+ */
+@Slf4j
+@RestController
+@RequestMapping("/curator")
+@Api(tags = "Curator测试", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class CuratorController {
+
+    @Autowired
+    private CuratorUtil curatorUtil;
+
+    @ApiOperation("创建临时节点")
+    @PostMapping("/node/ephemeral")
+    public String set(String node) throws Exception {
+        curatorUtil.createEphemeral(node);
+        return Constants.MSG_SUCCESS;
+    }
+}
---
+++ sample-zookeeper/src/main/java/com/cn/boot/sample/zookeeper/curator/CuratorUtil.java
@@ -0,0 +1,68 @@
+package com.cn.boot.sample.zookeeper.curator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Chen Nan
+ */
+@Slf4j
+@Component
+public class CuratorUtil {
+    private static CuratorFramework curator;
+    public static final String PARENT_NODE = "/test";
+
+    @PostConstruct
+    public void init() throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 10);
+
+        curator = CuratorFrameworkFactory.builder()
+                .connectString("127.0.0.1:2181")
+                .sessionTimeoutMs(3000)
+                .retryPolicy(retryPolicy)
+                .build();
+
+        curator.start();
+
+        Stat stat = curator.checkExists().forPath(PARENT_NODE);
+        if (stat == null) {
+            String result = curator.create().withMode(CreateMode.PERSISTENT).forPath(PARENT_NODE);
+            log.info("【CuratorUtil】PARENT_NODE create={}", result);
+        } else {
+            log.info("【CuratorUtil】PARENT_NODE stat={}", stat);
+        }
+
+        // 第三个参数表示是否需要返回所操作的子节点数据
+        PathChildrenCache cache = new PathChildrenCache(curator, PARENT_NODE, true);
+        cache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+
+        cache.getListenable().addListener((curatorFramework, event) -> {
+            switch (event.getType()) {
+                case CHILD_ADDED:
+                    log.info("【CuratorUtil】新增子节点" + event.getData().getPath());
+                    break;
+                case CHILD_UPDATED:
+                    log.info("【CuratorUtil】更新子节点" + event.getData().getPath());
+                    break;
+                case CHILD_REMOVED:
+                    log.info("【CuratorUtil】删除子节点" + event.getData().getPath());
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
+
+    public String createEphemeral(String node) throws Exception {
+        return curator.create().withMode(CreateMode.EPHEMERAL).forPath(PARENT_NODE + "/" + node);
+    }
+}
