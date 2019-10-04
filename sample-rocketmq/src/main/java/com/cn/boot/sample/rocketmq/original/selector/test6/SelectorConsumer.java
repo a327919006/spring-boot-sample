74718a8d15ce6c3ae761b95b1a9f310cd0a9db7f
chennan <327919006@qq.com>
2019-10-04 20:13:39 +0800

    整合RocketMQ-消息队列选择

---
+++ sample-rocketmq/src/main/java/com/cn/boot/sample/rocketmq/original/selector/test6/SelectorConsumer.java
@@ -0,0 +1,58 @@
+package com.cn.boot.sample.rocketmq.original.selector.test6;
+
+import cn.hutool.json.JSONUtil;
+import com.cn.boot.sample.rocketmq.constant.MqConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * selector，指定发送到的队列
+ *
+ * @author Chen Nan
+ */
+@Slf4j
+@Component
+public class SelectorConsumer {
+    public static final String TAG = "test6";
+
+    private static DefaultMQPushConsumer consumer;
+
+    @PostConstruct
+    public void init() throws MQClientException {
+        log.info("【SelectorConsumer】init");
+
+        consumer = new DefaultMQPushConsumer(TAG + "_consumer_group");
+
+        consumer.setNamesrvAddr(MqConstant.NAME_SERVER_ADDRESS);
+
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+
+        consumer.subscribe(TAG + "_topic", "*");
+
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            MessageExt msg = msgs.get(0);
+            log.info("【SelectorConsumer】msg={}", JSONUtil.toJsonStr(msg));
+            String body = new String(msg.getBody());
+
+            log.info("【SelectorConsumer】body={}", body);
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+
+        consumer.start();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        log.info("【SelectorConsumer】destroy");
+        consumer.shutdown();
+    }
+}
---
+++ sample-rocketmq/src/main/java/com/cn/boot/sample/rocketmq/original/selector/test6/SelectorProducer.java
@@ -0,0 +1,69 @@
+package com.cn.boot.sample.rocketmq.original.selector.test6;
+
+import cn.hutool.json.JSONUtil;
+import com.cn.boot.sample.rocketmq.constant.MqConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.List;
+
+/**
+ * selector，指定发送到的队列
+ *
+ * @author Chen Nan
+ */
+@Slf4j
+@Component
+public class SelectorProducer {
+    public static final String TAG = "test6";
+
+    private static DefaultMQProducer producer;
+
+    @PostConstruct
+    public void init() throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
+        log.info("【SelectorProducer】init");
+
+        producer = new DefaultMQProducer(TAG + "_producer_group");
+
+        producer.setNamesrvAddr(MqConstant.NAME_SERVER_ADDRESS);
+
+        producer.start();
+
+        // 发送五条消息
+        for (int i = 0; i < 5; i++) {
+            byte[] body = ("Hello RocketMQ! " + TAG + "_" + i).getBytes();
+            Message message = new Message();
+            message.setTopic(TAG + "_topic"); // 主题
+            message.setTags(TAG); // 标签
+            message.setKeys(TAG + "_" + i); // 消息唯一标识，用户自定义
+            message.setBody(body);
+
+            SendResult sendResult = producer.send(message, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    log.info("【SelectorProducer】size = " + mqs.size());
+                    Integer num = (Integer) arg;
+                    int queueNum = num % mqs.size();
+                    return mqs.get(queueNum);
+                }
+            }, i);
+            log.info("【SelectorProducer】sendResult = {}", JSONUtil.toJsonStr(sendResult));
+        }
+    }
+
+    @PreDestroy
+    public void destroy() throws Exception {
+        log.info("【SelectorProducer】destroy");
+        producer.shutdown();
+    }
+}
