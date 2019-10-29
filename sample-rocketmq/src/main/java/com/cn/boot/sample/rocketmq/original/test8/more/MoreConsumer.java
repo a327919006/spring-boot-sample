dd93cfa55691be1e3c602fafffd25c84baa4116d
chennan <chennan@onemt.com.cn>
2019-10-29 11:45:22 +0800

    整合RocketMQ，订阅多个topic

---
+++ sample-rocketmq/src/main/java/com/cn/boot/sample/rocketmq/original/test8/more/MoreConsumer.java
@@ -0,0 +1,60 @@
+package com.cn.boot.sample.rocketmq.original.test8.more;
+
+import cn.hutool.json.JSONUtil;
+import com.cn.boot.sample.rocketmq.constant.MqConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * selector，指定发送到的队列
+ *
+ * @author Chen Nan
+ */
+@Slf4j
+@Component
+public class MoreConsumer {
+    public static final String TAG = "test8";
+
+    private static DefaultMQPushConsumer consumer;
+
+    @PostConstruct
+    public void init() throws MQClientException {
+        log.info("【MoreConsumer】init");
+
+        consumer = new DefaultMQPushConsumer(TAG + "_consumer_group");
+
+        consumer.setNamesrvAddr(MqConstant.NAME_SERVER_ADDRESS);
+
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+
+        // 订阅多个主题
+        consumer.subscribe(TAG + "_0_topic", "*");
+        consumer.subscribe(TAG + "_1_topic", "*");
+
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            MessageExt msg = msgs.get(0);
+            log.info("【MoreConsumer】msg={}", JSONUtil.toJsonStr(msg));
+            String body = new String(msg.getBody());
+
+            log.info("【MoreConsumer】body={}, tag={}", body, msg.getTags());
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+
+        consumer.start();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        log.info("【MoreConsumer】destroy");
+        consumer.shutdown();
+    }
+}
---
+++ sample-rocketmq/src/main/java/com/cn/boot/sample/rocketmq/original/test8/more/MoreProducer.java
@@ -0,0 +1,57 @@
+package com.cn.boot.sample.rocketmq.original.test8.more;
+
+import cn.hutool.json.JSONUtil;
+import com.cn.boot.sample.rocketmq.constant.MqConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * selector，指定发送到的队列
+ *
+ * @author Chen Nan
+ */
+@Slf4j
+@Component
+public class MoreProducer {
+    public static final String TAG = "test8";
+
+    private static DefaultMQProducer producer;
+
+    @PostConstruct
+    public void init() throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
+        log.info("【MoreProducer】init");
+
+        producer = new DefaultMQProducer(TAG + "_producer_group");
+
+        producer.setNamesrvAddr(MqConstant.NAME_SERVER_ADDRESS);
+
+        producer.start();
+
+        for (int i = 0; i < 10; i++) {
+            byte[] body = ("Hello RocketMQ! " + TAG + "_" + i).getBytes();
+            Message message = new Message();
+            // 发送不同topic
+            message.setTopic(TAG + "_" + (i%2) + "_topic");
+            message.setTags(TAG + "_" + (i%2));
+            message.setBody(body);
+
+            SendResult sendResult = producer.send(message);
+            log.info("【MoreProducer】sendResult = {}", JSONUtil.toJsonStr(sendResult));
+        }
+    }
+
+    @PreDestroy
+    public void destroy() throws Exception {
+        log.info("【MoreProducer】destroy");
+        producer.shutdown();
+    }
+}
