bc02509eb5e8defd7a070825c9b5361679850bc8
chennan <chennan@onemt.com.cn>
2019-11-25 19:18:01 +0800

    整合Redis

--- sample-redis/src/main/java/com/cn/boot/sample/redis/config/JedisConfig.java
+++ sample-redis/src/main/java/com/cn/boot/sample/redis/config/JedisConfig.java
@@ -23,8 +23,14 @@ public class JedisConfig {
     @Value("${spring.redis.timeout}")
     private int timeout;
 
+    @Value("${spring.redis.jedis.pool.max-active}")
+    private int maxActive;
+
     @Value("${spring.redis.jedis.pool.max-idle}")
     private int maxIdle;
+
+    @Value("${spring.redis.jedis.pool.min-idle}")
+    private int minIdle;
 
     @Value("${spring.redis.jedis.pool.max-wait}")
     private long maxWaitMillis;
@@ -35,18 +41,19 @@ public class JedisConfig {
     @Bean
     public JedisPool redisPoolFactory() {
         log.info("[JedisPool]init, host={}, port={}", host, port);
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxIdle(maxIdle);
-        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
-        // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
-        jedisPoolConfig.setBlockWhenExhausted(true);
-        // 是否启用pool的jmx管理功能, 默认true
-        jedisPoolConfig.setJmxEnabled(true);
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(maxActive);
+        config.setMaxIdle(maxIdle);
+        config.setMinIdle(minIdle);
+        config.setMaxWaitMillis(maxWaitMillis);
+        config.setTestOnBorrow(true);
+        config.setTestOnReturn(true);
+        config.setTestWhileIdle(true);
 
         if (StrUtil.isBlank(password)) {
-            return new JedisPool(jedisPoolConfig, host, port, timeout);
+            return new JedisPool(config, host, port, timeout);
         } else {
-            return new JedisPool(jedisPoolConfig, host, port, timeout, password);
+            return new JedisPool(config, host, port, timeout, password);
         }
     }
 }
