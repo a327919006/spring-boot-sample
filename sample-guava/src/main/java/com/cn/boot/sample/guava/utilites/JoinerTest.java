b99684c63597fcd65c59b76af2ea38aae511c651
chennan <327919006@qq.com>
2019-12-08 22:00:53 +0800

    整合Guava

--- sample-guava/src/main/java/com/cn/boot/sample/guava/utilites/JoinerTest.java
+++ sample-guava/src/main/java/com/cn/boot/sample/guava/utilites/JoinerTest.java
@@ -1,6 +1,11 @@
 package com.cn.boot.sample.guava.utilites;
 
+import com.google.common.base.Joiner;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Chen Nan
@@ -8,4 +13,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JoinerTest {
 
+    private final List list1 = Arrays.asList("aaa", "bbb", "ccc");
+    private final List list2 = Arrays.asList("aaa", "bbb", null);
+
+    @Test
+    public void test1() {
+        String result1 = Joiner.on("#").join(list1);
+        log.info("result1={}", result1);
+    }
 }
