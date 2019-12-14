9860f80152352e4996b2f91e58035688d10f362d
chennan <327919006@qq.com>
2019-12-14 21:51:13 +0800

    整合Guava

--- sample-guava/src/main/java/com/cn/boot/sample/guava/utilites/JoinerTest.java
+++ sample-guava/src/main/java/com/cn/boot/sample/guava/utilites/JoinerTest.java
@@ -1,11 +1,13 @@
 package com.cn.boot.sample.guava.utilites;
 
+import cn.hutool.core.util.StrUtil;
 import com.google.common.base.Joiner;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Chen Nan
@@ -13,8 +15,8 @@ import java.util.List;
 @Slf4j
 public class JoinerTest {
 
-    private final List list1 = Arrays.asList("aaa", "bbb", "ccc");
-    private final List list2 = Arrays.asList("aaa", "bbb", null);
+    private final List<String> list1 = Arrays.asList("aaa", "bbb", "ccc");
+    private final List<String> list2 = Arrays.asList("aaa", "bbb", null);
 
     @Test
     public void onJoin() {
@@ -41,6 +43,22 @@ public class JoinerTest {
     public void onAppendTo() {
         StringBuilder result = Joiner.on("#").appendTo(new StringBuilder(), list1);
         // aaa#bbb#ccc
+        log.info("result={}", result);
+    }
+
+    @Test
+    public void streamSkipNull() {
+        String result = list1.stream()
+                .filter(StrUtil::isNotBlank)
+                .collect(Collectors.joining("#"));
+        log.info("result={}", result);
+    }
+
+    @Test
+    public void streamDefault() {
+        String result = list2.stream()
+                .map(item -> StrUtil.isBlank(item) ? "default" : item)
+                .collect(Collectors.joining("#"));
         log.info("result={}", result);
     }
 }
