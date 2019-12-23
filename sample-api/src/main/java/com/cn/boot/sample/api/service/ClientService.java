86be9947ac1a2fc96ee4dbbc4774643cfaa59b67
chennan <chennan@onemt.com.cn>
2019-12-23 13:04:38 +0800

    新增服务接口

--- sample-api/src/main/java/com/cn/boot/sample/api/service/ClientService.java
+++ sample-api/src/main/java/com/cn/boot/sample/api/service/ClientService.java
@@ -6,5 +6,12 @@ import com.cn.boot.sample.api.model.po.Client;
  * @author Chen Nan
  */
 public interface ClientService extends BaseService<Client, String> {
+    /**
+     * 保存Client
+     *
+     * @param client client信息
+     * @return 操作结果
+     */
+    int saveClient(Client client);
 
 }
