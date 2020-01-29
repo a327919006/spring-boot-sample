132686ac47b02e6b848b225db4da4713d078dc02
chennan <327919006@qq.com>
2020-01-29 11:30:28 +0800

    整合mybatis、tk-mybatis

---
+++ sample-dal/src/main/java/com/cn/boot/sample/dal/mapper/OrderMapper.java
@@ -0,0 +1,10 @@
+package com.cn.boot.sample.dal.mapper;
+
+import com.cn.boot.sample.api.model.po.Order;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author Chen Nan
+ */
+public interface OrderMapper extends Mapper<Order> {
+}
