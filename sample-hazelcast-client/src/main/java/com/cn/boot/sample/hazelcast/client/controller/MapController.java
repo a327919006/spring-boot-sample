ed696e13b1339051773ae351551e13f4d5e91737
chennan <327919006@qq.com>
2020-01-29 11:31:16 +0800

    整合Hazelcast分布式缓存

--- sample-hazelcast-client/src/main/java/com/cn/boot/sample/hazelcast/client/controller/MapController.java
+++ sample-hazelcast-client/src/main/java/com/cn/boot/sample/hazelcast/client/controller/MapController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping("/map")
-@Api(tags = "MAP", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Api(tags = "MAP", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MapController {
 
     @Autowired
