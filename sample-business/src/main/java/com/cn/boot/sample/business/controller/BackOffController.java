6915be3597fdf00d35344fcb905b540ff4b082dd
chennan <chennan@onemt.com.cn>
2020-05-15 11:05:26 +0800

    新增测试接口，BackOff退避测试

--- sample-business/src/main/java/com/cn/boot/sample/business/controller/BackOffController.java
+++ sample-business/src/main/java/com/cn/boot/sample/business/controller/BackOffController.java
@@ -1,5 +1,6 @@
 package com.cn.boot.sample.business.controller;
 
+import cn.hutool.core.thread.ThreadUtil;
 import com.cn.boot.sample.api.model.Constants;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -7,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.util.backoff.BackOff;
 import org.springframework.util.backoff.BackOffExecution;
+import org.springframework.util.backoff.ExponentialBackOff;
 import org.springframework.util.backoff.FixedBackOff;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,13 +23,70 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "退避", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BackOffController {
 
-    @ApiOperation("测试")
-    @GetMapping("")
-    public String list() {
-        BackOff backOff = new FixedBackOff(1000, 3);
+    /**
+     * 固定间隔退避
+     *
+     * @param interval 间隔时间
+     * @param max      最大次数
+     */
+    @ApiOperation("FixedBackOff")
+    @GetMapping("/fixed")
+    public String fixedBackOff(long interval, int max) {
+        BackOff backOff = new FixedBackOff(interval, max);
         BackOffExecution execution = backOff.start();
 
-        execution.nextBackOff();
+        for (int i = 0; i <= max; i++) {
+            long value = execution.nextBackOff();
+            log.info("value={}", value);
+
+            if (value == BackOffExecution.STOP) {
+                break;
+            }
+            // 执行相关业务
+            log.info("test backoff={}", i);
+            boolean result = false;
+            // 如果业务执行成功，则退出
+            if (result) {
+                break;
+            }
+            // 业务执行失败，退避重试
+            ThreadUtil.sleep(value);
+        }
+
+        return Constants.MSG_SUCCESS;
+    }
+
+    /**
+     * 指数间隔退避
+     *
+     * @param interval   间隔时间
+     * @param multiplier 倍数
+     * @param max        最大次数
+     */
+    @ApiOperation("ExponentialBackOff")
+    @GetMapping("/exponential")
+    public String exponentialBackOff(long interval, int multiplier, int max) {
+        BackOff backOff = new ExponentialBackOff(interval, multiplier);
+        BackOffExecution execution = backOff.start();
+
+        for (int i = 0; i < max; i++) {
+            long value = execution.nextBackOff();
+            log.info("value={}", value);
+
+            if (value == BackOffExecution.STOP) {
+                break;
+            }
+            // 执行相关业务
+            log.info("test backoff={}", i);
+            boolean result = false;
+            // 如果业务执行成功，则退出
+            if (result) {
+                break;
+            }
+            // 业务执行失败，退避重试
+            ThreadUtil.sleep(value);
+        }
+
         return Constants.MSG_SUCCESS;
     }
 }
