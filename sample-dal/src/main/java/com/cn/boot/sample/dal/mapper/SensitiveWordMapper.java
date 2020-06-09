48f714851ff03dc4fd36768498c8adb9764e0864
chennan <chennan@onemt.com.cn>
2020-06-09 11:34:29 +0800

    整合mybatis、tk-mybatis

---
+++ sample-dal/src/main/java/com/cn/boot/sample/dal/mapper/SensitiveWordMapper.java
@@ -0,0 +1,10 @@
+package com.cn.boot.sample.dal.mapper;
+
+import com.cn.boot.sample.api.model.po.SensitiveWord;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author Chen Nan
+ */
+public interface SensitiveWordMapper extends Mapper<SensitiveWord> {
+}
