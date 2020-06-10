9cb96c3370ff910ca3b114a2b6758f0d2dee79c4
chennan <chennan@onemt.com.cn>
2020-06-10 19:54:03 +0800

    新增测试接口-MongoDB动态数据源测试

--- sample-business/src/main/java/com/cn/boot/sample/business/controller/MessageController.java
+++ sample-business/src/main/java/com/cn/boot/sample/business/controller/MessageController.java
@@ -25,7 +25,7 @@ public class MessageController {
     @Reference
     private MessageService messageService;
 
-    @ApiOperation("消息-添加")
+    @ApiOperation("添加")
     @PostMapping("")
     public Message insert(@RequestBody Message req) {
         req.setCreateTime(LocalDateTime.now());
@@ -33,32 +33,40 @@ public class MessageController {
         return messageService.insert(req);
     }
 
-    @ApiOperation("消息-列表")
+    @ApiOperation("添加消息-动态数据源")
+    @PostMapping("/dynamic")
+    public Message insertDynamic(@RequestBody Message req, @RequestHeader String appId) {
+        req.setCreateTime(LocalDateTime.now());
+        req.setUpdateTime(LocalDateTime.now());
+        return messageService.insertMessage(appId, req);
+    }
+
+    @ApiOperation("获取列表")
     @GetMapping("/list")
     public List<Message> find(@ModelAttribute Message req) {
         return messageService.list(req);
     }
 
-    @ApiOperation("消息-获取")
+    @ApiOperation("根据ID获取")
     @GetMapping("/{id}")
     public Message find(@PathVariable String id) {
         return messageService.selectByPrimaryKey(id);
     }
 
-    @ApiOperation("消息-删除")
+    @ApiOperation("删除")
     @DeleteMapping("/{id}")
     public int delete(@PathVariable String id) {
         return messageService.deleteByPrimaryKey(id);
     }
 
-    @ApiOperation("消息-更新")
+    @ApiOperation("更新")
     @PutMapping("/")
     public int update(@RequestBody Message req) {
         req.setUpdateTime(LocalDateTime.now());
         return messageService.updateByPrimaryKey(req);
     }
 
-    @ApiOperation("消息-数量")
+    @ApiOperation("获取数量")
     @GetMapping("/count/{day}")
     public List<MessageCount> count(@PathVariable Long day) {
         return messageService.findMessageCount(LocalDateTime.now().minusDays(day), 1);
