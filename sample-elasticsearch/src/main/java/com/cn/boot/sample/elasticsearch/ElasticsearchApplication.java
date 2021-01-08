8127b54439080cf49e225c4b9df8a75b2f5a1b93
ChenNan <600700@git.nd.com>
2021-01-08 17:25:52 +0800

    整合Elasticsearch7.10

---
+++ sample-elasticsearch/src/main/java/com/cn/boot/sample/elasticsearch/ElasticsearchApplication.java
@@ -0,0 +1,14 @@
+package com.cn.boot.sample.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Chen Nan
+ */
+@SpringBootApplication
+public class ElasticsearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class, args);
+    }
+}
