5c9aa8531dc4a670c7e9ba89b72218eb4d8ab48b
chennan <327919006@qq.com>
2021-01-18 22:56:21 +0800

    整合SpringBootActuator

---
+++ sample-actuator/src/main/java/com/cn/boot/sample/actuator/ActuatorApplication.java
@@ -0,0 +1,14 @@
+package com.cn.boot.sample.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Chen Nan
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+}
