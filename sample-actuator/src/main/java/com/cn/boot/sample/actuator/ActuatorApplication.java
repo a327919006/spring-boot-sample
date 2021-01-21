76f39869cc8f2437d0d92549f25e407f5ed81680
chennan <327919006@qq.com>
2021-01-21 23:14:36 +0800

    整合SpringBootActuator、Micrometer、Prometheus

--- sample-actuator/src/main/java/com/cn/boot/sample/actuator/ActuatorApplication.java
+++ sample-actuator/src/main/java/com/cn/boot/sample/actuator/ActuatorApplication.java
@@ -2,11 +2,13 @@ package com.cn.boot.sample.actuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Chen Nan
  */
 @SpringBootApplication
+@EnableScheduling
 public class ActuatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
