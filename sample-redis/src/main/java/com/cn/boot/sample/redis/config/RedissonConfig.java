306a3f35ace0716f61207dd93ab0999741c27e4b
chennan <327919006@qq.com>
2021-01-24 23:35:19 +0800

    整合Redis、SpringDataRedis、Jedis、Redisson

---
+++ sample-redis/src/main/java/com/cn/boot/sample/redis/config/RedissonConfig.java
@@ -0,0 +1,20 @@
+package com.cn.boot.sample.redis.config;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Chen Nan
+ */
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public Redisson redisson() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379").setDatabase(0);
+        return (Redisson) Redisson.create(config);
+    }
+}
--- sample-redis/src/main/java/com/cn/boot/sample/redis/controller/LockTestController.java
+++ sample-redis/src/main/java/com/cn/boot/sample/redis/controller/LockTestController.java
@@ -8,6 +8,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Reference;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,8 +29,12 @@ public class LockTestController {
 
     @Reference
     private RedisService redisService;
+    @Autowired
+    private Redisson redisson;
 
-    @ApiOperation("测试")
+    private int num = 0;
+
+    @ApiOperation("测试(有瑕疵，不会自动延期)")
     @GetMapping("")
     public String lock(String key, int count) {
         ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
@@ -49,6 +56,35 @@ public class LockTestController {
                         log.info("out");
                         redisService.unlock(key);
                     }
+                }
+            });
+        }
+
+        return Constants.MSG_SUCCESS;
+    }
+
+    @ApiOperation("测试（使用Redisson会自动延期）")
+    @GetMapping("redisson")
+    public String redissonLock(String key, int count) {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNamePrefix("test")
+                .build();
+        // 确认线程池参数
+        ExecutorService executor = new ThreadPoolExecutor(500, 500,
+                60000L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(102400), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+
+        for (int i = 0; i < count; i++) {
+            executor.execute(() -> {
+                RLock redissonLock = redisson.getLock(key);
+                redissonLock.lock();
+                try {
+                    num++;
+                    log.info("num={}", num);
+                    ThreadUtil.sleep(10);
+                } finally {
+                    log.info("out");
+                    redissonLock.unlock();
                 }
             });
         }
