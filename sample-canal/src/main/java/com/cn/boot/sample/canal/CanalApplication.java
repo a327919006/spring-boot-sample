f24a6ffbc2217291ca7cf4d4dea8e7a17b901d9c
ChenNan <600700@git.nd.com>
2021-02-02 10:31:34 +0800

    整合Canal实现binlog日志订阅

---
+++ sample-canal/src/main/java/com/cn/boot/sample/canal/CanalApplication.java
@@ -0,0 +1,15 @@
+package com.cn.boot.sample.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Chen Nan
+ */
+@SpringBootApplication
+public class CanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+}
