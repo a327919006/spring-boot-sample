9f8d9cf9ee4642ee2639979fa6523fad41d44a38
chennan <327919006@qq.com>
2021-07-22 22:22:37 +0800

    整合Jackson

--- sample-jackson/src/main/java/com/cn/boot/sample/jackson/JacksonApplication.java
+++ sample-jackson/src/main/java/com/cn/boot/sample/jackson/JacksonApplication.java
@@ -1,11 +1,13 @@
 package com.cn.boot.sample.jackson;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Chen Nan
  */
+@EnableSwagger2Doc
 @SpringBootApplication
 public class JacksonApplication {
     public static void main(String[] args) {
