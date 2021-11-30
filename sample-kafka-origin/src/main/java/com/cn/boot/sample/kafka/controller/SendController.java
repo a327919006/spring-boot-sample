0faa953ed74c68dcee85c249d560fdb14e6d07b6
chennan <nan.chen@cntepower.com>
2021-11-30 09:35:00 +0800

    整合Kafka，原生API

--- sample-kafka-origin/src/main/java/com/cn/boot/sample/kafka/controller/SendController.java
+++ sample-kafka-origin/src/main/java/com/cn/boot/sample/kafka/controller/SendController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,13 @@ public class SendController {
     @PostMapping
     public String send(String msg) {
         testProducer.send(KafkaConstant.TOPIC_TEST, msg);
+        return msg;
+    }
+
+    @ApiOperation("发送消息")
+    @PostMapping("{topic}")
+    public String send(@PathVariable String topic, String msg) {
+        testProducer.send(topic, msg);
         return msg;
     }
 
