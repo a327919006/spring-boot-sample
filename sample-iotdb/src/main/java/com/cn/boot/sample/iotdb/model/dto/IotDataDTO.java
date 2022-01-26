2b1997a1391aff02eabd16928244b2db8f97f689
chennan <nan.chen@cntepower.com>
2022-01-26 10:28:43 +0800

    整合IotDB

--- sample-iotdb/src/main/java/com/cn/boot/sample/iotdb/model/dto/IotDataDTO.java
+++ sample-iotdb/src/main/java/com/cn/boot/sample/iotdb/model/dto/IotDataDTO.java
@@ -1,5 +1,6 @@
 package com.cn.boot.sample.iotdb.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -12,5 +13,6 @@ public class IotDataDTO {
     private String deviceId;
     private List<String> measurements;
     private List<String> values;
+    @ApiModelProperty(value = "多久之前，单位毫秒", required = false, notes = "默认为获取1小时内数据")
     private Long before;
 }
