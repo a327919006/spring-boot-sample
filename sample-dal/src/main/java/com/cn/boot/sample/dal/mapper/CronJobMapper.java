f294c2f798421300adab9cf922cbe1aa856fa79d
chennan <nan.chen@cntepower.com>
2022-02-25 17:33:52 +0800

    整合mybatis、tk-mybatis

---
+++ sample-dal/src/main/java/com/cn/boot/sample/dal/mapper/CronJobMapper.java
@@ -0,0 +1,10 @@
+package com.cn.boot.sample.dal.mapper;
+
+import com.cn.boot.sample.api.model.po.CronJob;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CronJobMapper extends Mapper<CronJob> {
+}
