204e4c8f3bf23666c55f510bbec3bb43ab753b91
chennan <nan.chen@cntepower.com>
2022-09-14 14:48:13 +0800

    新增测试接口

---
+++ sample-business/src/main/java/com/cn/boot/sample/business/controller/AlertmanagerController.java
@@ -0,0 +1,28 @@
+package com.cn.boot.sample.business.controller;
+
+import com.cn.boot.sample.api.model.dto.alert.AlertDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Chen Nan
+ */
+@Slf4j
+@RestController
+@RequestMapping("/alert")
+@Api(tags = "测试AlertManager WebHook", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AlertmanagerController {
+
+    @ApiOperation("")
+    @PostMapping("")
+    public void insert(@RequestBody AlertDTO req) {
+        log.info("req = {}", req);
+    }
+
+}
