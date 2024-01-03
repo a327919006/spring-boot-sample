8584a40d653184d2bf919737538e8eb914366f3a
chennan <nan.chen@cntepower.com>
2024-01-03 20:04:13 +0800

    整合Guava

--- sample-guava/src/main/java/com/cn/boot/sample/guava/io/CommitInfo.java
+++ sample-guava/src/main/java/com/cn/boot/sample/guava/io/CommitInfo.java
@@ -1,9 +1,12 @@
 package com.cn.boot.sample.guava.io;
 
+import cn.hutool.core.date.DateUtil;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Chen Nan
@@ -12,8 +15,22 @@ import java.util.List;
 public class CommitInfo {
     private String type;
     private String num;
-    private List<String> authorList;
+    private Set<String> authorList;
     private Date first;
     private Date last;
 
+    @Override
+    public String toString() {
+        String typeStr = "";
+        if (StringUtils.equals(type, CommitParseTest.typeFeat)) {
+            typeStr = "需求";
+        } else {
+            typeStr = "BUG";
+        }
+        return typeStr + "\t"
+                + num + "\t"
+                + DateUtil.formatDate(first) + "\t"
+                + DateUtil.formatDate(last) + "\t"
+                + authorList;
+    }
 }
