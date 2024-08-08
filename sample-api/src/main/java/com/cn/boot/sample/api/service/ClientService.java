c81e885ddb9f186b86e217d35f47fceb9dbbc78c
chennan <327919006@qq.com>
2024-08-08 23:06:08 +0800

    新增AOP测试API

--- sample-api/src/main/java/com/cn/boot/sample/api/service/ClientService.java
+++ sample-api/src/main/java/com/cn/boot/sample/api/service/ClientService.java
@@ -14,4 +14,11 @@ public interface ClientService extends BaseService<Client, String> {
      */
     int saveClient(Client client);
 
+    /**
+     * 测试事务
+     *
+     * @param client client信息
+     * @return 操作结果
+     */
+    int testAop(Client client);
 }
