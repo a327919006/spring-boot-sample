21cf823f9c15274f56dd8de2e54e424da00cdd30
chennan <327919006@qq.com>
2024-08-24 22:21:44 +0800

    新增Redis缓存测试接口

--- sample-business/src/main/java/com/cn/boot/sample/business/controller/RedisController.java
+++ sample-business/src/main/java/com/cn/boot/sample/business/controller/RedisController.java
@@ -2,9 +2,8 @@ package com.cn.boot.sample.business.controller;
 
 import com.cn.boot.sample.api.exceptions.BusinessException;
 import com.cn.boot.sample.api.model.po.Client;
+import com.cn.boot.sample.api.service.CacheService;
 import com.cn.boot.sample.api.service.ClientService;
-import com.cn.boot.sample.api.service.RedisService;
-import com.cn.boot.sample.api.service.UidGeneratorService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -21,23 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/redis")
-@Api(tags = "测试13-Redis", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(tags = "测试13-Redis缓存", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RedisController {
 
     @Reference
-    private RedisService redisService;
+    private CacheService cacheService;
     @Reference
     private ClientService clientService;
-    @Reference
-    private UidGeneratorService uidGeneratorService;
-
-    @ApiOperation("锁")
-    @GetMapping("/set/{key}")
-    public String set(@PathVariable String key) {
-        String value = uidGeneratorService.generate();
-        redisService.lock(key, value);
-        return value;
-    }
 
     @ApiOperation("缓存对象")
     @GetMapping("/client/cache/{id}")
@@ -47,19 +36,19 @@ public class RedisController {
             throw new BusinessException("商户不存在");
         }
 
-        redisService.cache(id, client);
+        cacheService.cache(id, client);
         return client;
     }
 
     @ApiOperation("获取对象")
     @GetMapping("/{key}")
     public Object get(@PathVariable String key) {
-        return redisService.get(key);
+        return cacheService.get(key);
     }
 
     @ApiOperation("获取商户")
     @GetMapping("/client/{id}")
     public Client getClient(@PathVariable String id) {
-        return redisService.getClient(id);
+        return cacheService.getClient(id);
     }
 }
