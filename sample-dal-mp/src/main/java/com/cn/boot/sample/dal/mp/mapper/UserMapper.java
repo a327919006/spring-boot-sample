e16e235d19a28f14ac8da51c6664fc4b2bc4f4b8
chennan <327919006@qq.com>
2024-09-05 23:02:41 +0800

    整合MyBatisPlus

---
+++ sample-dal-mp/src/main/java/com/cn/boot/sample/dal/mp/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.cn.boot.sample.dal.mp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cn.boot.sample.api.model.po.User;
+
+/**
+ * @author Chen Nan
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
