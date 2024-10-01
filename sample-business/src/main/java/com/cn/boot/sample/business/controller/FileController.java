1ad9ca06aa12227414befa94fc0b9c1e782620da
chennan <327919006@qq.com>
2024-10-01 22:45:37 +0800

    整合XEasyPdf

--- sample-business/src/main/java/com/cn/boot/sample/business/controller/FileController.java
+++ sample-business/src/main/java/com/cn/boot/sample/business/controller/FileController.java
@@ -22,6 +22,11 @@ import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.tools.zip.ZipEntry;
 import org.apache.tools.zip.ZipOutputStream;
+import org.dromara.pdf.fop.core.base.Template;
+import org.dromara.pdf.fop.core.doc.Document;
+import org.dromara.pdf.fop.core.doc.component.text.Text;
+import org.dromara.pdf.fop.core.doc.page.Page;
+import org.dromara.pdf.fop.handler.TemplateHandler;
 import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -34,10 +39,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author ChenNan
@@ -286,6 +288,18 @@ public class FileController {
 
         writeDate(list, fileName, ClientCsv.class, response);
     }
+
+    @ApiOperation("下载PDF")
+    @GetMapping("/download/pdf")
+    public void downloadPdf(HttpServletResponse response) throws Exception {
+        Document document = TemplateHandler.Document.build();
+        Page page = TemplateHandler.Page.build();
+        Text text = TemplateHandler.Text.build().setText("hello world");
+        page.addBodyComponent(text);
+        document.addPage(page);
+        document.transform("./hello-world.pdf");
+    }
+
 
     private <T> void writeDate(List<T> list, String fileName, Class<T> clazz, HttpServletResponse response) throws Exception {
         ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream(1024 * 1024);
