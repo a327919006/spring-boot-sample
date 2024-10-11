8fbffed55de8320df9a5b9eaa3d0170fad2070ae
chennan <327919006@qq.com>
2024-10-11 22:45:25 +0800

    整合JFreeChart

--- sample-business/src/main/java/com/cn/boot/sample/business/util/chart/ChartUtil.java
+++ sample-business/src/main/java/com/cn/boot/sample/business/util/chart/ChartUtil.java
@@ -2,15 +2,20 @@ package com.cn.boot.sample.business.util.chart;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.jfree.chart.*;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.StandardChartTheme;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.title.LegendTitle;
 import org.jfree.chart.title.TextTitle;
+import org.jfree.chart.title.Title;
 import org.jfree.chart.ui.HorizontalAlignment;
 import org.jfree.chart.ui.RectangleEdge;
 import org.jfree.chart.ui.RectangleInsets;
+import org.jfree.chart.ui.VerticalAlignment;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.xy.XYSeries;
@@ -24,6 +29,8 @@ import java.io.File;
  */
 @Slf4j
 public class ChartUtil {
+
+    private static final Color COLOR_GRAY = new Color(146, 146, 146);
 
     /**
      * 创建饼图
@@ -51,54 +58,66 @@ public class ChartUtil {
     @SneakyThrows
     public static void createLineChart() {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.setValue(10, "技术部", "2021年");
-        dataset.setValue(30, "技术部", "2022年");
-        dataset.setValue(55, "技术部", "2023年");
-        dataset.setValue(200, "技术部", "2024年");
-        dataset.setValue(1, "人事部", "2021年");
-        dataset.setValue(5, "人事部", "2022年");
-        dataset.setValue(10, "人事部", "2023年");
-        dataset.setValue(15, "人事部", "2024年");
-        dataset.setValue(10, "销售部", "2021年");
-        dataset.setValue(20, "销售部", "2022年");
-        dataset.setValue(35, "销售部", "2023年");
-        dataset.setValue(50, "销售部", "2024年");
+        dataset.setValue(10, "BUG", "01月");
+        dataset.setValue(25, "BUG", "02月");
+        dataset.setValue(30, "BUG", "03月");
+        dataset.setValue(40, "BUG", "04月");
+        dataset.setValue(55, "BUG", "05月");
+        dataset.setValue(60, "BUG", "06月");
+        dataset.setValue(70, "BUG", "07月");
+        dataset.setValue(87, "BUG", "08月");
+        dataset.setValue(93, "BUG", "09月");
+        dataset.setValue(15, "漏洞", "01月");
+        dataset.setValue(24, "漏洞", "02月");
+        dataset.setValue(35, "漏洞", "03月");
+        dataset.setValue(46, "漏洞", "04月");
+        dataset.setValue(55, "漏洞", "05月");
+        dataset.setValue(68, "漏洞", "06月");
+        dataset.setValue(79, "漏洞", "07月");
+        dataset.setValue(83, "漏洞", "08月");
+        dataset.setValue(94, "漏洞", "09月");
 
         StandardChartTheme chartTheme = new StandardChartTheme("CN");
-        chartTheme.setExtraLargeFont(new Font("微软雅黑", Font.BOLD, 13));
-        chartTheme.setRegularFont(new Font("微软雅黑", Font.BOLD, 10));
-        chartTheme.setLargeFont(new Font("微软雅黑", Font.BOLD, 10));
+        chartTheme.setExtraLargeFont(new Font("微软雅黑", Font.PLAIN, 13));
+        chartTheme.setRegularFont(new Font("微软雅黑", Font.PLAIN, 10));
+        chartTheme.setLargeFont(new Font("微软雅黑", Font.PLAIN, 10));
         ChartFactory.setChartTheme(chartTheme);
         JFreeChart chart = ChartFactory.createLineChart(null, null, null, dataset);
         CategoryPlot plot = (CategoryPlot) chart.getPlot();
         plot.setBackgroundPaint(Color.WHITE);  // 设置绘图区背景颜色为白色
 
         // 自定义标题
-        TextTitle customTitle = new TextTitle("部门人数曲线", new Font("微软雅黑", Font.BOLD, 13));
+        TextTitle customTitle = new TextTitle("风险点趋势", new Font("微软雅黑", Font.BOLD, 12));
         customTitle.setHorizontalAlignment(HorizontalAlignment.LEFT);
-        // 减少标题周围的边距，使其更靠近左侧
         customTitle.setMargin(new RectangleInsets(5, 5, 5, 5)); // 可以根据需要调整这些值
         chart.setTitle(customTitle);
 
-        // 网格线
-        plot.setRangeGridlinesVisible(true);
-        plot.setRangeGridlinePaint(Color.LIGHT_GRAY);
-
-        // 边框
-        plot.setOutlineVisible(false);
-
-        // 获取X轴和Y轴
-        CategoryAxis domainAxis = plot.getDomainAxis();
-        domainAxis.setAxisLineVisible(false);
-        domainAxis.setTickMarksVisible(false);
-        ValueAxis rangeAxis = plot.getRangeAxis();
-        rangeAxis.setAxisLineVisible(false);
-        rangeAxis.setTickMarksVisible(false);
-
-        // 设置图例位置
+        // 设置图例位置-右上角（默认下方中间）
         LegendTitle legend = chart.getLegend();
         legend.setPadding(0, 300, 0, 0);
         legend.setPosition(RectangleEdge.TOP);
+
+        // 增加y轴单位说明文字
+        Title title = new TextTitle("单位：个", new Font("微软雅黑", Font.BOLD, 10), COLOR_GRAY,
+                RectangleEdge.TOP, HorizontalAlignment.LEFT, VerticalAlignment.TOP, RectangleInsets.ZERO_INSETS);
+        title.setPadding(0, 20, 0, 0);
+        chart.addSubtitle(title);
+
+        // 增加网格线
+        plot.setRangeGridlinePaint(COLOR_GRAY);
+
+        // 隐藏边框
+        plot.setOutlineVisible(false);
+
+        // 隐藏x轴和y轴的标尺
+        CategoryAxis domainAxis = plot.getDomainAxis();
+        domainAxis.setAxisLineVisible(false);
+        domainAxis.setTickMarksVisible(false);
+        domainAxis.setTickLabelPaint(COLOR_GRAY);
+        ValueAxis rangeAxis = plot.getRangeAxis();
+        rangeAxis.setAxisLineVisible(false);
+        rangeAxis.setTickMarksVisible(false);
+        rangeAxis.setTickLabelPaint(COLOR_GRAY);
 
         ChartUtils.saveChartAsPNG(new File("./pdf/chart/lineChart.png"), chart, 500, 200);
     }
