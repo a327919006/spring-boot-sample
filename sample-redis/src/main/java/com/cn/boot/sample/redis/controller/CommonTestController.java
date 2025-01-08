cb3d68c0c2cf58b331f82d7d3da67e03ede21e4b
chennan <nan.chen@cntepower.com>
2025-01-08 19:15:47 +0800

    整合Redis、SpringDataRedis、Jedis、Redisson、Lua

--- sample-redis/src/main/java/com/cn/boot/sample/redis/controller/CommonTestController.java
+++ sample-redis/src/main/java/com/cn/boot/sample/redis/controller/CommonTestController.java
@@ -1,12 +1,17 @@
 package com.cn.boot.sample.redis.controller;
 
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.cn.boot.sample.api.model.Constants;
+import com.cn.boot.sample.api.model.dto.RspBase;
+import com.cn.boot.sample.api.model.po.Client;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RAtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,13 +29,15 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @RestController
 @RequestMapping("/common/test")
-@Api(tags = "基础功能测试", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(tags = "1、基础功能测试", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CommonTestController {
 
     @Autowired
     private JedisPool jedisPool;
     @Autowired
     private RAtomicLong atomicLong;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Value("${spring.redis.database}")
     private int database;
@@ -50,5 +57,19 @@ public class CommonTestController {
         return StrUtil.fillBefore(num, '0', 4);
     }
 
+    @ApiOperation("3、测试RedisTemplate写入json")
+    @PostMapping("/redisTemplate/json/set")
+    public String jsonSet(Client client) {
+        String json = JSONObject.toJSONString(client);
+        redisTemplate.opsForValue().set("clientTest", json);
+        return Constants.MSG_SUCCESS;
+    }
 
+    @ApiOperation("4、测试RedisTemplate读取json")
+    @GetMapping("/redisTemplate/json/get")
+    public RspBase<Client> jsonGet(String key) {
+        String json = (String) redisTemplate.opsForValue().get(key);
+        Client client = JSONObject.parseObject(json, Client.class);
+        return RspBase.data(client);
+    }
 }
